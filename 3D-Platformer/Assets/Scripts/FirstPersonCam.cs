68284818667f8d52bd7ba7310d2f44805d47b713
AlexCipriano <aciprian@uncc.edu>
2018-02-01 12:37:02 -0500

    Added Camera and character controller
    added camera and controller movement. Tied the camera to the collider and added movement to control them

--- 3D-Platformer/Assets/Scripts/FirstPersonCam.cs
+++ 3D-Platformer/Assets/Scripts/FirstPersonCam.cs
@@ -3,16 +3,30 @@ using System.Collections;
 
 public class FirstPersonCam : MonoBehaviour {
 
-	public float speedH = 2.0f;
-	public float speedV = 2.0f;
+    public float minimumX = -60.0f;
+    public float maximumX = 60.0f;
+    public float minimumY = -360.0f;
+    public float maximumY = 360.0f;
+    public float sensitivityX = 15.0f;
+    public float sensitivityY = 15.0f;
 
-	private float yaw = 0.0f;
-	private float pitch = 0.0f;
+    public Camera cam;
 
-	void Update () {
-		yaw += speedH * Input.GetAxis("Mouse X");
-		pitch -= speedV * Input.GetAxis("Mouse Y");
+    float rotationX;
+    float rotationY;
 
-		transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update () {
+
+        rotationY += Input.GetAxis("Mouse X") * sensitivityY;
+        rotationX += Input.GetAxis("Mouse Y") * sensitivityX;
+
+        rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
+
+        transform.localEulerAngles = new Vector3(0, rotationY, 0);
+        cam.transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0);
 	}
 }
