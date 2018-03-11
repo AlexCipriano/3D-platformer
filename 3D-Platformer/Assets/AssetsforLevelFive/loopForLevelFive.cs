6ee190eb76613098de3208daf9ff7f7259cbb713
Matt Aftimos <mattaftimos@gmail.com>
2018-03-11 12:52:27 -0400

    LevelFive&LevelSix Update // Check Description
    ~~LEVELFIVE UPDATES~~
    
    -Added new script that allows prefabs to move up and down in the Y direction, file named "loopforLevelFive"
    **Need to fix loopforLevelFive to countine moving if the player jumps on that prefab**
    -Added the framework for this level
    -Added assets for levelSix
    -Added top down design for LevelFive
    -Fixed prefab collision for one asset that wasnt correct
    -Added more reflection on water
    -Added more wave collision to give a realistic feeling
    
    ~~Will need to fix color and lighting for this level eventually
    ~~Add traps and fix corner ratio;s for some assets
    ~~Addd more to the level with time
    
    ~~LEVELSIX UPDATES~~
    
    -Added new scene labeled "LevelSix"
    -Added new folder to contain all asets for LevelSix
    -Added snowman asset
    -Added snow feet sound to the FPS controller for LevelSix
    -Added blizzard sound to the entire level
    -Added snow sound to walking, running, jumping and landing
    -Added skybox for level, might need improvement
    -Added multiple snowman to the level for fun
    
    ~~This current layout is just mockup, i fixed enviromental changes so suit this level design for future implementation
    ~~Want to make this level enclosued with traps moving in x direction or y direction
    ~~Add a better skybox if avaiable
    ~~Add killzone to all levels
    ~~Make sure the sound is correct and nothing is too loud compared to others
    ~~Brush the folders to get rid of unused data for all asssets within this project
    ~~Follow up on fps controllers
    
    \\Test the level out and give feedback

---
+++ 3D-Platformer/Assets/AssetsforLevelFive/loopForLevelFive.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+public class loopForLevelFive : MonoBehaviour {
+
+    private Vector3 MovingDirection = Vector3.up;
+
+    void Update() {
+        gameObject.transform.Translate(MovingDirection * Time.smoothDeltaTime);
+
+        if (gameObject.transform.position.y > 72) {
+            MovingDirection = Vector3.down;
+        }
+        else if (gameObject.transform.position.y < 61) {
+            MovingDirection = Vector3.up;
+        }
+    }
+}
