e341cb6d4f615aea1498ac1caeab98994d62e844
Matt Aftimos <mattaftimos@gmail.com>
2018-03-26 18:09:38 -0400

    Update // Check Description
    **LevelOne**
    -Added directional light to make level brighter
    
    **LevelFour**
    -Added walls to parts of the level
    -Made the level abit longer so you can look down
    -Added a skybox, galaxy effect
    -When you look down at the start of the game you see space on the bottom
    -Added light throughout the level
    -Fixed a flame issue with a prefab that looks kinda funky
    -Added floor lights throughout the level
    -Fixed overlapping meshes on some area ( might be more)
    -Fixed one place on the top side where you couldnt access
    -Gave the Level a dungeon feel
    -Encapsulated some parts of the levels like previously stated
    -Fixed one trap that was off of its wall
    -Added lanterns to the top of the level
    
    **LevelFive**
    -Added a mirror so you cant see the entire level
    -Mirror is low pixel, i tried running a script to fix it, no luck
    -Mirror is good for now
    -Added scripts in the LevelFive folder of assets.

---
+++ 3D-Platformer/Assets/SkyboxAssets/SkyBox HDR/examplescene.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class examplescene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		RenderSettings.skybox = (Material)Resources.Load("Skybox3");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnGUI() {
+		int x = 50;
+		int y = 50;
+		int dy = 40;
+		int cnt = 0;
+		int sx = 300;
+		int sy = 30;
+		if (GUI.Button(new Rect(x, y+dy*cnt++, sx, sy), "Skybox 1 - hubble deep field")) {
+			RenderSettings.skybox = (Material)Resources.Load("Skybox1");
+		}
+		
+		
+	}
+}
