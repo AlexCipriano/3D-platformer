47f5e7aa3da6d1979cb4e7d469f9b4c0f77ee028
Matt Aftimos <mattaftimos@gmail.com>
2018-04-04 18:39:57 -0400

    I think its finally fixed

---
+++ 3D-platformer/3D-Platformer/Assets/AssetsforLevelFive/loopForLevelFive.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+public class loopForLevelFive : MonoBehaviour {
+
+    private Vector3 MovingDirection = Vector3.forward;
+
+    void Update() {
+        gameObject.transform.Translate(MovingDirection * Time.smoothDeltaTime);
+
+        if (gameObject.transform.position.z > -200) {
+            MovingDirection = Vector3.back;
+        }
+        else if (gameObject.transform.position.z < -2000) {
+            MovingDirection = Vector3.forward;
+        }
+    }
+}
---
+++ 3D-platformer/3D-Platformer/Assets/Scripts/Characters/CharacterStats.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CharacterStats : MonoBehaviour {
+
+	[Range(0, 10)] public float health = 10;
+	public int faction;
+	public int gemCount;
+	public Text gemText;
+	public Text hpText;
+
+	// Use this for initialization
+	void Start () {
+		updateGems ();
+		updateHealth ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		health = Mathf.Clamp (health, 0, 5);
+		updateGems ();
+		updateHealth ();
+	}
+
+	void updateGems() {
+		gemText.text = "Gems: " + gemCount.ToString();
+	}
+
+	void updateHealth() {
+		hpText.text = "Health: " + health.ToString();
+	}
+
+}
