1be3c6ff91ec94fe38182ea09e45037e08097f38
Matt Aftimos <mattaftimos@gmail.com>
2018-04-13 22:57:06 -0400

    desc
    New script for footstep sounds for the controller model, need to be fixed though the volume is too high

---
+++ 3D-Platformer/Assets/Foot.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Foot : MonoBehaviour {
+
+    // Use this for initialization
+    CharacterController cc;
+    void Start() {
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (cc.isGrounded == true && cc.velocity.magnitude > 2f && GetComponent<AudioSource>().isPlaying == false) {
+            GetComponent<AudioSource>().volume = 0.2f;
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
