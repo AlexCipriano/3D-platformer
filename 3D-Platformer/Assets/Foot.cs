cb4195b2a8d3dc05ffd83e2c62205297d19628b1
Matt Aftimos <mattaftimos@gmail.com>
2018-04-14 10:21:54 -0400

    Added sound too all controllers for walking

--- 3D-Platformer/Assets/Foot.cs
+++ 3D-Platformer/Assets/Foot.cs
@@ -1,19 +1,24 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class Foot : MonoBehaviour {
 
-    // Use this for initialization
+    AudioSource audioSource;
+
     CharacterController cc;
     void Start() {
+        audioSource = GetComponent<AudioSource>();
         cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update() {
-        if (cc.isGrounded == true && cc.velocity.magnitude > 2f && GetComponent<AudioSource>().isPlaying == false) {
-            GetComponent<AudioSource>().volume = 0.2f;
-            GetComponent<AudioSource>().Play();
+        if (cc.isGrounded == true && cc.velocity.magnitude > 2f && audioSource.isPlaying == false) {
+            audioSource.volume = Random.Range(0.2f, 0.4f);
+            audioSource.pitch = Random.Range(0.7f, 0.9f);
+            audioSource.Play();
         }
     }
 }
