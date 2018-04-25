cd3dfca8ece29e854eeca01841278241425913c5
AlexCipriano <akatsukimember5550@hotmail.com>
2018-04-25 18:58:28 -0400

    Added Pause menu
    Added a pause menu to the canvas

---
+++ 3D-Platformer/Assets/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public static bool paused;
+	public GameObject pauseMenu;
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			if (paused) {
+				Resume ();
+			} else 
+			{
+				Pause();
+			}
+		}
+
+		
+	}
+
+
+	public void Resume()
+	{
+		pauseMenu.SetActive (false);
+		Time.timeScale = 1f;
+		paused = false;
+	}
+
+	void Pause ()
+	{
+		pauseMenu.SetActive (true);
+		Time.timeScale = 0f;
+		paused = true;
+	}
+}
--- 3D-Platformer/Assets/menuController.cs
+++ 3D-Platformer/Assets/menuController.cs
@@ -4,6 +4,28 @@ using UnityEngine;
 
 public class menuController : MonoBehaviour {
 
+	public static menuController instance = null;
+
+
+	void Awake () 
+	{
+		//Check if instance already exists
+		if (instance == null)
+
+			//if not, set instance to this
+			instance = this;
+
+		//If instance already exists and it's not this:
+		else if (instance != this)
+
+			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+			Destroy(gameObject);    
+
+		//Sets this to not be destroyed when reloading scene
+		DontDestroyOnLoad(gameObject);
+
+	}
+
 	// Use this for initialization
 	void Start () {
         Cursor.lockState = CursorLockMode.Confined;
